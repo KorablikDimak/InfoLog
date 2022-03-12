3610d7fbef508a68c0127d2559f7e70cf5892632
korab <korablikdimak@gmail.com>
2022-03-12 03:31:21 +0300

    assembly

--- obj/Debug/net5.0/InfoLog.AssemblyInfo.cs
+++ obj/Debug/net5.0/InfoLog.AssemblyInfo.cs
@@ -11,13 +11,18 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("InfoLog")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("KorablikDimak")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCopyrightAttribute("KorablikDimak@gmail.com")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("A package for creating a log with the ability to supplement various logging optio" +
+    "ns and use the built-in logging to the console, file, database.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("InfoLog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InfoLog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/KorablikDimak/InfoLog")]
+[assembly: System.Resources.NeutralResourcesLanguageAttribute("en")]
 
 // Создано классом WriteCodeFragment MSBuild.
 
